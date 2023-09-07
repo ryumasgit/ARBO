ecf7f8df96e10e73df80ccbfae4e8280d5a1a074
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-09-07 04:58:29 +0000

    add public sesstion

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :member_state, only: [:create]
+
+  def after_sign_in_path_for(resource)
+    reviews_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -13,7 +23,7 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # DELETE /resource/sign_out
+  # # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
@@ -24,4 +34,13 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  protected
+
+  def member_state
+    @member = Member.find_by(email: params[:email])
+    return if !@member
+    if @member.valid_password?(params[:email][:password]) && (@member.is_active == false)
+      redirect_to new_member_registration_path, notice: "退会済みのメンバーです。新規登録をしてご利用ください。"
+    end
+  end
 end

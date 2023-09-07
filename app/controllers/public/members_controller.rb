34071cbdcbf0342c2b9a9594fc3cd379acbbaba9
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-09-07 12:05:07 +0000

    add-public-member

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,7 +1,9 @@
 class Public::MembersController < ApplicationController
   before_action :authenticate_member!
-  
+
   def show
+    @member = Member.find_by(member_name: params[:member_member_name])
+    redirect_if_member_not_found(@member)
   end
 
   def edit
@@ -14,5 +16,14 @@ class Public::MembersController < ApplicationController
   end
 
   def withdraw
+  end
+  
+  protected
+  
+  def redirect_if_member_not_found(member)
+    if member.nil?
+      flash[:alert] = "メンバーが見つかりません。"
+      redirect_to root_path
+    end
   end
 end

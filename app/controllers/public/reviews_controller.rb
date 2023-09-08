6acd80caf5ad504c4a72a530a2af4168266e6302
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-09-08 12:43:49 +0000

    add-public-review

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,6 +1,7 @@
 class Public::ReviewsController < ApplicationController
   before_action :authenticate_member!
-  
+  before_action :ensure_correct_member,  except: [:show, :index]
+
   def new
   end
 
@@ -11,6 +12,8 @@ class Public::ReviewsController < ApplicationController
   end
 
   def index
+    members = Member.where(is_active: true)
+    @reviews = members.first.reviews
   end
 
   def edit
@@ -20,5 +23,14 @@ class Public::ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+  protected
+
+  def ensure_correct_member
+    @member = Member.find_by(member_name: params[:member_member_name])
+    unless @member == current_member
+    redirect_to reviews_path
+    end
   end
 end

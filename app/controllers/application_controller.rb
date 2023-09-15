19d9af7fb7e1682794c6f82bc14e122c393ff4a3
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-09-15 14:35:23 +0000

    add public review comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -59,7 +59,19 @@ class ApplicationController < ActionController::Base
     redirect_to admin_reviews_path
     return
   end
-
+  
+  def public_review_comment_handle_record_not_found
+    set_flash_message("指定されたURLは見つかりませんでした")
+    redirect_to reviews_path
+    return
+  end
+  
+  def admin_review_comment_handle_record_not_found
+    set_flash_message("指定されたURLは見つかりませんでした")
+    redirect_to admin_reviews_path
+    return
+  end
+  
   def member_is_guest?
     if current_member.is_guest?
       set_flash_message("権限がありません ブロックされました")
--- app/controllers/public/review_comments_controller.rb
+++ app/controllers/public/review_comments_controller.rb
@@ -1,13 +1,31 @@
 class Public::ReviewCommentsController < ApplicationController
   before_action :authenticate_member!
   before_action :member_is_guest?, only: [:create, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :public_review_comment_handle_record_not_found
 
   def index
+    @review = Review.find(params[:review_id])
+    @review_comments = @review.review_comments
+    @review_comment = ReviewComment.new
   end
 
   def create
+    # @review_comment = ReviewComment.new(review_comment_params)
+    # @review = ReviewComment.find(params[:review_id])
+    # review_comment = current_member.review_comments.new(review_comment_params)
+    # review_comment.review_id = @review.id
+    # review_comment.save
   end
 
   def destroy
+  #   review_comment = ReviewComment.find(params[:id])
+  #   review_comment.destroy
+  #   @book = Book.find(params[:book_id])
+  end
+
+  private
+
+  def review_comment_params
+    params.require(:review_comment).permit(:comment)
   end
 end

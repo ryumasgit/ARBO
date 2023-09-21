619aa79cca7dd417220754f608a60ed9a48d2be9
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-09-21 14:51:49 +0000

    add public review

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,7 @@ Rails.application.routes.draw do
       resource :bookmark_exhibitions, only: [:create, :destroy]
     end
     resources :searches, only: [:index]
+    get 'searches/:tag', to: 'searches#index', as: :tag_search
     resources :artists, only: [:show]
     resources :badges, only: [:show]
   end

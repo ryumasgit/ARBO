511676e1b2af61c6dbc23b8656c142c7f78aef3e
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-09-24 09:53:08 +0000

    add form error

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -12,11 +12,29 @@ class Public::RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
     if resource.member_image.blank?
-      flash[:notice] =  "画像が最低1つは必要です"
+      flash[:notice] =  ["画像が最低1つは必要です"]
       redirect_to new_member_registration_path
       return
     end
-    super
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      flash[:notice] = resource.errors.full_messages
+      redirect_to new_member_registration_path
+    end
   end
 
   protected

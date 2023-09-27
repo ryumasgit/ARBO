835515ca524db2727e1897839ea73e460cbea61f
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-09-27 07:22:38 +0000

    fix notification

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -2,10 +2,10 @@ class Public::NotificationsController < ApplicationController
   before_action :authenticate_member!
 
   def index
-    @notifications = current_member.passive_notifications.page(params[:page]).per(20)
-    @notifications = @notifications.joins(:visitor).where(members: { is_active: true })
-    @notifications.where(checked: false).each do |notification|
+    notifications = current_member.passive_notifications.page(params[:page]).per(20)
+    notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
+    @notifications = notifications.joins(:visitor).where(members: { is_active: true })
   end
 end

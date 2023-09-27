82396670be49d438f06880badc9a167aa73dd179
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-09-27 07:00:10 +0000

    fix member is_active false display

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,11 +4,15 @@ class Public::SearchesController < ApplicationController
   def index
     if params[:content].present?
       content = params[:content]
-      @members = Member.where("name LIKE ?", "%#{content}%").page(params[:page])
-      @reviews = Review.where("body LIKE ?", "%#{content}%").page(params[:page])
+      @members = Member.where("name LIKE ? AND is_active = ?", "%#{content}%", true).page(params[:page])
+      @reviews = Review.joins(:member).where(members: { is_active: true }).where("body LIKE ?", "%#{content}%").page(params[:page])
     elsif params[:tag].present?
       @members = []
-      @reviews = Review.joins(:tags).where(tags: { name: params[:tag] }).page(params[:page])
+      @reviews = Review.joins(:tags)
+               .joins(:member)
+               .where(tags: { name: params[:tag] })
+               .where(members: { is_active: true })
+               .page(params[:page])
     else
       @members = []
       @reviews = []
